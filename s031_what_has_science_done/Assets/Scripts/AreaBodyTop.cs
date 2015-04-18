3df7fea84422d072815322a6aa31963549ba40af
Tobias Wehrum <Tobias.Wehrum@dragonlab.de>
2015-04-18 23:23:52 +0200

    New parts Added AreaBodyTop to Torso parts

---
+++ s031_what_has_science_done/Assets/Scripts/AreaBodyTop.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[RequireComponent(typeof(EdgeCollider2D))]
+public class AreaBodyTop : MonoBehaviourBase
+{
+}
